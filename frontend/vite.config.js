016c7d899e006651d47168841c1400c1f5f41237
tito3004 <79288456+tito3004@users.noreply.github.com>
2025-12-14 22:28:59 -0500

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/mi-proyecto-ci-cd-express-vite/',
   plugins: [
     react({
       babel: {

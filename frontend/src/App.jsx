4d0765c2f94238dfff115e2480a0a0eb36bce202
tito3004 <79288456+tito3004@users.noreply.github.com>
2025-12-14 23:07:39 -0500

    Backend render

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,7 +5,7 @@ function App() {
 
   useEffect(() => {
     // Ajusta la URL luego según dónde corra el backend
-    fetch("http://localhost:4000/api/saludo")
+    fetch("https://mi-proyecto-ci-cd-express-vite-jo3d.onrender.com/api/saludo")
       .then((res) => res.json())
       .then((data) => setMensaje(data.mensaje))
       .catch(() =>
